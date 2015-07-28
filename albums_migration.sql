e6fd22d56a054ca96c4334042843a479f3b42c68
David <d_rod3663@hotmail.com>
2015-07-28 13:30:21 -0500

    first commit:

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,
+release_date DATE NOT NULL,
+sales DECIMAL(7,2)NOT NULL,
+genre VARCHAR(50) NOT NULL,
+PRIMARY KEY (id)
+);
